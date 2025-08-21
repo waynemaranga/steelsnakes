c700ec4ca31aea4d4647845e76df4f25e716817c
WM <waynemaranga@gmail.com>
2025-08-21 23:46:16 +0300

    AUS

---
+++ src/steelsnakes/core/sections/AUS/__init__.py
@@ -0,0 +1,13 @@
+"""
+Australian Steel Sections Module.
+
+This module will provide access to Australian steel sections conforming to AS standards.
+Currently not implemented - planned for future development.
+"""
+
+# TODO: Implement Australian steel sections following AS 4100 standards
+
+
+__all__ = [
+    # Will be populated when Australian sections are implemented
+]
